bd693d478a9a661d9120b14e62beabf641389315
clintar <clint@gurutechnologies.net>
2016-09-22 13:58:54 -0600

    get rid of old fantomcoin cruft

--- src/cryptonote_core/cryptonote_basic.h
+++ src/cryptonote_core/cryptonote_basic.h
@@ -456,7 +456,6 @@ namespace cryptonote
 
     BEGIN_SERIALIZE()
       VARINT_FIELD(major_version)
-      if(major_version > BLOCK_MAJOR_VERSION_2) return false;
       VARINT_FIELD(minor_version)
       VARINT_FIELD(timestamp)
       FIELD(prev_id)
@@ -473,11 +472,6 @@ namespace cryptonote
 
     BEGIN_SERIALIZE_OBJECT()
       FIELDS(*static_cast<block_header *>(this))
-//      if (BLOCK_MAJOR_VERSION_2 <= major_version)
-//      {
-//        auto sbb = make_serializable_bytecoin_block(*this, false, false);
-//        FIELD_N("parent_block", sbb);
-//      }
       FIELD(miner_tx)
       FIELD(tx_hashes)
     END_SERIALIZE()
